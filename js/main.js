8900824a0fe927e753ef51e57b602b3835c99b8d
yyhhenry <yyhhenry@foxmail.com>
2019-07-26 13:24:31 +0800

    school

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ window.onload=function(){
 	let live=new Live(document.getElementById('live'));
 	(function anim(){
 		live.paint();
-		requestAnimationFrame(anim);
+		setTimeout(anim,10);
 	})();
 }
--- js/ptImg.js
+++ js/ptImg.js
@@ -3,35 +3,35 @@ PtImg=function(filename){
 	let img;
 	let canvas;
 	let ctx;
-	let loaded=false;
 	img=document.createElement('img');
 	img.src=filename;
 	let ans=[];
+	let imgload=false;
 	img.onload=function(){
+		imgload=true;
 		canvas=document.createElement('canvas');
 		canvas.width=img.width;
 		canvas.height=img.height;
 		ctx=canvas.getContext('2d');
 		ctx.clearRect(0,0,canvas.width,canvas.height);
-		//ctx.drawImage(img,0,0);
-		loaded=true;
+		ctx.drawImage(img,0,0);
 	}
-	this.getWidth=function(){
-		return img.width;
+	this.cLoad=function(){
+		if(ans.length>=img.height*img.width)return;
+		if(imgload){
+			let time=new Date().getTime();
+			while(new Date().getTime()-time<5){
+				let g=ans.length;
+				let y=g%img.height;
+				let x=(g-y)/img.height;
+				let v=ctx.getImageData(x,y,1,1).data;
+				ans[g]=((v[0]*256+v[1])*256+v[2])*256+v[3];
+			}
+		}
 	}
-	this.getHeight=function(){
-		return img.height;
-	}
-	let ak=false;
 	this.getColor=function(i,j){
-		if(!loaded)return 0;
-		i=parseInt(i);
-		j=parseInt(j);
-		if(i<0||j<0||i>=img.width||j>=img.height)return 0;
 		let g=i*img.height+j;
 		if(ans[g]!=null)return ans[g];
-		let v=ctx.getImageData(i,j,1,1).data;
-		ans[g]=((v[0]*256+v[1])*256+v[2])*256+v[3];
-		return ans[g];
+		else return 0;
 	}
 }
