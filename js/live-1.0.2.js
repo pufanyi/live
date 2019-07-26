c1d52265172301de028aab995dd51680b28c3c6e
yyhhenry <yyhhenry@foxmail.com>
2019-07-26 15:50:29 +0800

    school

--- js/live-1.0.2.js
+++ js/live-1.0.2.js
@@ -28,13 +28,19 @@ Live=function(element){
 	
 	let 身;
 	let 头;
+	let dq;
 	this.initAct=function(){
 		身=new PtImg(imgBase.身);
 		头=new PtImg(imgBase.头);
+		dq=1;
 	}
 	this.runAct=function(time){
 		身.cLoad();
 		头.cLoad();
+		let dis=Math.abs(mouse.x-WIDTH/2);
+		dq+=time*(dis-130)/1000000;
+		if(dq>1.2)dq=1.2;
+		if(dq<1)dq=1;
 	}
 	$(window).mousemove(function(event){
 		mouse={
@@ -51,6 +57,7 @@ Live=function(element){
 		const recolor=['','0','00','000','0000','00000','000000','0000000','00000000'];
 		let drawPoint=function(x,y,color){
 			if(color==0)return;
+			if(mouse.x*2>WIDTH)x=WIDTH-x;
 			if(x>=0&&x<WIDTH&&y>=0&&y<HEIGHT){
 				color=color.toString(16);
 				color='#'+recolor[8-color.length]+color;
@@ -58,31 +65,17 @@ Live=function(element){
 				ctx.fillRect(x,y,1,1);
 			}
 		}
-		if(mouse.x*2>WIDTH){
-			for(let x=0;x<WIDTH;x++){
-				for(let y=0;y<HEIGHT;y++){
-					drawPoint(x,y,身.getColor(WIDTH-x,y));
-				}
+		for(let x=0;x<WIDTH;x++){
+			for(let y=0;y<HEIGHT;y++){
+				drawPoint(x,y,身.getColor(x,y));
 			}
-			let q=5;
-			for(let x=0;x<WIDTH;x++){
-				for(let y=0;y<HEIGHT;y++){
-					let p=q*Math.pow(y,0.666);
-					drawPoint(x,y,头.getColor(WIDTH-(p+1)/p*x+mouse.x/p,y));
-				}
-			}
-		}else{
-			for(let x=0;x<WIDTH;x++){
-				for(let y=0;y<HEIGHT;y++){
-					drawPoint(x,y,身.getColor(x,y));
-				}
-			}
-			let q=5;
-			for(let x=0;x<WIDTH;x++){
-				for(let y=0;y<HEIGHT;y++){
-					let p=q*Math.pow(y,0.666);
-					drawPoint(x,y,头.getColor((p+1)/p*x-mouse.x/p,y));
-				}
+		}
+		let l=110;
+		let r=180;
+		let bg=Math.floor(r-(r-l)*dq);
+		for(let x=bg;x<r;x++){
+			for(let y=0;y<HEIGHT;y++){
+				drawPoint(x,y,头.getColor((x-bg)/dq+l,y));
 			}
 		}
 		lastPaintTime=thisPaintTime;
